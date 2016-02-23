daaaee9eecee27ba00dbb0261306264e074d8d06
Aleksi <aleksi.kallio@outlook.com>
2016-02-23 18:38:04 +0200

    Start reservation api

--- models/reservation.js
+++ models/reservation.js
@@ -1,16 +1,42 @@
-var mongoose = require('mongoose');
-var Event = require('./event');
+const mongoose = require('mongoose');
+const Event = require('./event');
+const async = require('async');
 
-var reservationSchema = Event.discriminator( 'Reservation', new mongoose.Schema({
-	startDate: {
-		type: Date, 
-		required: true 
-	},
-	endDate: {
-		type: Date,
-		required: true,
-	}
-
+const reservationSchema = Event.discriminator('Reservation', new mongoose.Schema({
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+    required: true,
+  },
 }));
 
-module.exports = mongoose.model('InfoPost', infoPostSchema);
+reservationSchema.pre('validate', (next, done) => {
+  const reservation = this;
+  if (reservation.endDate < reservation.startDate) {
+    done(new Error('End date cannot be before start date'));
+  }
+
+  async.parallel([
+    function findBeforeAfter(callback) {
+      mongoose.models.Reservation
+        .find()
+        .where('startDate').gt(reservation.startDate)
+        .or('endDate').lt(reservation.endDate).count(callback);
+    },
+    function findAll(callback) {
+      mongoose.models.Reservation
+        .findAll().count(callback);
+    },
+  ], (err, results) => {
+    if (results[0] !== results[1]) {
+      done(new Error('Overlapping reservation'));
+    }
+
+    next();
+  });
+});
+
+module.exports = mongoose.model('Reservation', reservationSchema);
---
+++ routes/reservation.js
@@ -0,0 +1,157 @@
+const Reservation = require('../models/reservation');
+const passport = require('./passport.js');
+
+module.exports = (app) => {
+  app.get('/api/reservations', (req, res) => {
+    Reservation.find({}, (err, reservations) => {
+      if (err) {
+        res.sendStatus(500);
+        return;
+      }
+      if (reservations.length === 0) {
+        res.status(404).send({
+          message: 'No reservations found',
+        });
+        return;
+      }
+      res.json(reservations);
+    });
+  });
+
+  /*
+   * GET reservation information
+   */
+  app.get('/api/reservations/:reservationId', (req, res) => {
+    Reservation.findById(req.params.reservationId,
+      '_id title startDate endDate comments userId createdAt', (err, reservation) => {
+        if (err) {
+          res.status(500).send({
+            message: 'Reservation not found',
+          });
+          return;
+        }
+        if (reservation === null) {
+          res.status(404).send({
+            message: 'Reservation not found',
+          });
+          return;
+        }
+        res.json(reservation);
+      });
+  });
+
+  /*
+   * POST add new reservation
+   */
+  app.post('/api/reservations', passport.authenticate('jwt', {
+    session: false,
+  }), (req, res) => {
+    const addReservation = req.body;
+    addReservation.userId = req.user._id;
+
+    if (addReservation.title === undefined ||
+        addReservation.startDate === undefined ||
+        addReservation.endDate === undefined) {
+      res.status(400).send({
+        message: 'Invalid reservation data',
+      });
+      return;
+    }
+
+    const newReservation = new Reservation(addReservation);
+
+    newReservation.save((err, reservation) => {
+      if (err) {
+        res.status(500).send({
+          message: 'Creation failed',
+        });
+        return;
+      }
+
+      res.status(201).json({
+        id: reservation.id,
+      });
+    });
+  });
+
+  /*
+   * PUT update task
+   */
+  app.put('/api/reservations/:taskId', passport.authenticate('jwt', {
+    session: false,
+  }), (req, res) => {
+    const updatedReservation = req.body;
+
+    if (updatedReservation.title === undefined || updatedReservation.category === undefined) {
+      res.status(400).send({
+        message: 'Invalid task data',
+      });
+      return;
+    }
+
+    Reservation.findById(req.params.taskId, (err, task) => {
+      if (err) {
+        res.sendStatus(500);
+        return;
+      }
+
+      if (!task) {
+        res.status(404).send({
+          message: 'Reservation not found',
+        });
+        return;
+      }
+
+      if (!req.user._id.equals(task.userId)) {
+        res.status(403).send({
+          message: 'Unauthorized',
+        });
+        return;
+      }
+
+      task.update(updatedReservation, (err2) => {
+        if (err2) {
+          res.sendStatus(500);
+          return;
+        }
+        res.sendStatus(200);
+      });
+    });
+  });
+
+  /*
+   * DELETE task
+   */
+  app.delete('/api/reservations/:reservationId', passport.authenticate('jwt', {
+    session: false,
+  }), (req, res) => {
+    Reservation.findById(req.params.reservationId, (err, reservation) => {
+      if (err) {
+        res.sendStatus(500);
+        return;
+      }
+
+      if (!reservation) {
+        res.status(404).send({
+          message: 'Reservation not found',
+        });
+        return;
+      }
+
+      if (!req.user._id.equals(reservation.userId)) {
+        res.status(403).send({
+          message: 'Unauthorized',
+        });
+        return;
+      }
+
+      reservation.remove((err2) => {
+        if (err2) {
+          res.sendStatus(500);
+          return;
+        }
+        res.sendStatus(200);
+      });
+    });
+  });
+};
