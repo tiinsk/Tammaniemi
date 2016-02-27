ad1bc472f537e846e3427001af8797a948db47ea
Aleksi <aleksi.kallio@outlook.com>
2016-02-27 21:02:18 +0200

    Refactor user model to ES6

--- models/user.js
+++ models/user.js
@@ -1,49 +1,55 @@
-var mongoose = require('mongoose');
-var bcrypt = require('bcrypt-nodejs');
-var SALT_WORK_FACTOR = 10;
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+const SALT_WORK_FACTOR = 10;
 
-var userSchema = new mongoose.Schema({
-	name: {type: String, required: true },
-	email: {type: String, required: true },
-	password: {type: String, required: true }
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 });
 
 userSchema.pre('save', function(next) {
-    var user = this;
+  const user = this;
 
-	// only hash the password if it has been modified (or is new)
-	if (!user.isModified('password')) return next();
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) next();
 
-	// generate a salt
-	bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-	    if (err) {
-	    	return next(err);
-	    }
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+    if (err) {
+      next(err);
+    }
 
-	    // hash the password using our new salt
-	    bcrypt.hash(user.password, salt, null, function(err, hash) {
-	        if (err) return next(err);
-	        // override the cleartext password with the hashed one
-	        user.password = hash;
-	        next();
-	    });
-	});
+    // hash the password using our new salt
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      if (err) next(err);
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+    });
+  });
 });
 
-userSchema.methods.comparePassword = function(candidatePassword, cb) {
-    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-        if (err) return cb(err);
-        cb(null, isMatch);
-    });
+userSchema.methods.comparePassword = (candidatePassword, cb) => {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    if (err) cb(err);
+    cb(null, isMatch);
+  });
 };
 
-userSchema.methods.isPasswordValid = function(candidatePassword) {
-    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-        return isMatch;
-    });
+userSchema.methods.isPasswordValid = (candidatePassword) => {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    isMatch();
+  });
 };
-
-
-
 
 module.exports = mongoose.model('User', userSchema);
