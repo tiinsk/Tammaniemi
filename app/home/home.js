78bcdc21029c1b96a823e57d8d21c2e37994d817
Aleksi <aleksi.kallio@outlook.com>
2016-03-16 19:37:53 +0200

    New home

--- app/home/home.js
+++ app/home/home.js
@@ -1,38 +1,92 @@
+import moment from 'moment';
 import React from 'react';
+
+import EventStore from '../events/event_store';
+import Event from '../events/event_layout';
+
+import Task from '../events/task/task_layout';
 
 import Row from 'muicss/lib/react/row';
 import Col from 'muicss/lib/react/col';
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: this.sortEvents(EventStore.getByType('events'))
+    };
+    this.onChange = this.onChange.bind(this);
+  }
+
+  componentDidMount() {
+    EventStore.listen(this.onChange);
+  }
+
+  componentWillUnmount() {
+    EventStore.unlisten(this.onChange);
+  }
+
+  onChange(state) {
+    this.setState({
+      events: this.sortEvents(state.events)
+    });
+  }
+
+  sortEvents(events) {
+    return events.sort((a, b) => {
+      const aDate = moment(a.createdAt);
+      const bDate = moment(b.createdAt);
+      if (aDate.isAfter(bDate)) {
+        return -1;
+      }
+      if (aDate.isBefore(bDate)) {
+        return 1;
+      }
+      // a must be equal to b
+      return 0;
+    });
+  }
+
+  handleDelete() {
+    return true;
+  }
+
+  handleUpdate() {
+    return true;
+  }
+
   render() {
+    const events = this.state.events.map((event) => {
+      const type = event.__t.toLowerCase();
+      if (type === 'task') {
+        return (
+          <Task key={event._id}
+            task={event}
+            to={`/tasks/${event._id}`}
+            delete={this.handleDelete}
+            update={this.handleUpdate}
+          />
+        );
+      }
+      return (
+        <Event key={event._id} className={type}
+          event={event}
+          to={`/${type}s/${event._id}`}
+          delete={this.handleDelete}
+          update={this.handleUpdate}
+        >
+          {event.content}
+        </Event>
+      );
+    });
 
     return (
-      <div>
-          <Row>
-            <Col lg="8" style={{"border": "1px solid black","minHeight": "500px"}}>
-              <Row>
-                <Col lg="6" style={{"border": "1px solid black","height": "250px"}}>
-                  <div>Posts</div>
-
-                </Col>
-                <Col lg="6" style={{"border": "1px solid black","height": "250px"}}>
-                  <div>Tasks</div>
-                </Col>
-              </Row>
-              <Row>
-                <Col lg="6" style={{"border": "1px solid black","height": "250px"}}>
-                  <div>Imgs</div>
-                </Col>
-                <Col lg="6" style={{"border": "1px solid black","height": "250px"}}>
-                  <div>Infoposts</div>
-                </Col>
-              </Row>
-
-            </Col>
-            <Col lg="4" style={{"border": "1px solid black","height": "500px"}}>
-              <div>Calendar</div>
-            </Col>
-          </Row>
+      <div className="container">
+        <Row>
+          <Col md="6" md-offset="3" >
+            {events}
+          </Col>
+        </Row>
       </div>
     );
   }
