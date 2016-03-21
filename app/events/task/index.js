daee66a04285e82111ec6b9f5adb7e449ebb5ca0
tiinsk <tiina.koskiranta@gmail.com>
2016-03-21 20:20:00 +0200

    task index updated

--- app/events/task/index.js
+++ app/events/task/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import _ from 'lodash';
 import EventActions from '../event_actions';
 import EventStore from '../event_store';
 import Row from 'muicss/lib/react/row';
@@ -7,7 +7,7 @@ import Col from 'muicss/lib/react/col';
 
 import history from '../../history';
 
-import Task from './task_layout';
+import Task_S from './task_layout_s';
 
 class IndexTasks extends React.Component {
   constructor(props) {
@@ -43,23 +43,74 @@ class IndexTasks extends React.Component {
     history.pushState(null, `/tasks/update/${taskId}`);
   }
 
-  render() {
-    const taskList = this.state.tasks.map((task) =>
-      (
-        <Task key={task._id}
-          task={task}
-          to={`/tasks/${task._id}`}
-          delete={this.handleDelete}
-          update={this.handleUpdate}
-        />
-      )
-    );
+  handleChecked(taskId){
+    let updatedTask = this.state.tasks.find((task) => task._id === taskId );
+    updatedTask.isDone = !updatedTask.isDone;
 
+    EventActions.update({type: 'tasks', content: updatedTask});
+  }
+
+  getTasksByCategory(){
+    const grouped = this.state.tasks.reduce((result, task) => {
+      if (!result[task.category]) {
+        result[task.category] = [];
+      }
+      result[task.category].push(
+        <Task_S key={task._id}
+            task={task}
+            to={`/tasks/${task._id}`}
+            delete={this.handleDelete}
+            update={this.handleUpdate}
+            toggleChecked={this.handleChecked.bind(this)}
+          />
+      );
+      return result;
+    }, {});
+    return grouped;
+  }
+
+  render() {
+    const grouped = this.getTasksByCategory();
     return (
-      <div className="container">
+      <div className="index-layout">
+          <div md="6" className="header-section">
+              <div className="title" > Tasks </div>
+              <div className="new-task">
+              <div className="symbol">
+                <div className="img" ></div>
+              </div>
+              <div className="header"> Add new task </div>
+            </div>
+          </div>
         <Row>
-          <Col md="6" md-offset="3" >
-            {taskList}
+          <Col className="column" lg="6">
+            <div className="section">
+              <div className="symbol symbol_buy"></div>
+              <h2 className="header">To Buy</h2>
+              {grouped[0]}
+            </div>
+            <div className="section">
+              <div className="symbol symbol_fix"></div>
+              <h2 className="header">Fix These</h2>
+              {grouped[2]}
+            </div>
+            <div className="section">
+              <div className="symbol symbol_other"></div>
+              <h2 className="header">Others</h2>
+              {grouped[4]}
+            </div>
+          </Col>
+          <Col className="column" lg="6">
+            <div className="section">
+              <div className="symbol symbol_food"></div>
+              <h2 className="header">Food to Buy</h2>
+              {grouped[1]}
+            </div>
+            <div className="section">
+              <div className="symbol symbol_nature"></div>
+              <h2 className="header">Leafy Things</h2>
+              {grouped[3]}
+            </div>
           </Col>
         </Row>
 
---
+++ app/events/task/task_layout_s.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import {Link} from 'react-router';
+import moment from 'moment';
+
+import CommentBox from '../comment/comments';
+
+export default class Task_S extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      "isCommentsShown": false
+    }
+  }
+
+  toggleComments(){
+    this.setState({
+      "isCommentsShown": !this.state.isCommentsShown
+    })
+  }
+
+  toggleChecked(){
+    this.props.toggleChecked(this.props.task._id)
+  }
+
+  render() {
+    let title;
+    if(this.props.to != null){
+      title = (
+        <div className="title-container">
+          <Link className={"title " + (this.props.task.isDone ? "overlined": "") } to={this.props.to}>{this.props.task.title}</Link>
+        </div>
+        );
+    }else{
+      title= (
+        <div className={"title " + (this.props.task.isDone ? "overlined": "") } >{this.props.task.title}</div>
+        );
+    }
+
+    let commentBox;
+    if (this.state.isCommentsShown) {
+      commentBox = (
+        <CommentBox comments={this.props.task.comments} eventId={this.props.task._id} addComment={this.props.addComment} />
+        );
+    }
+
+    return (
+      <div className="task-s" >
+        <div className="box">
+          <div className="primary-symbol">
+            <div className="chekbox">
+              <div onClick={this.toggleChecked.bind(this)} className={"checkmark " + (this.props.task.isDone ? "checked" : "") } ></div>
+            </div>
+          </div>
+          {title}
+          <div className="details" >
+            <span className="user">{this.props.task.userId.name}</span>
+            <span className="created-at" >{moment(this.props.task.createdAt).fromNow()}</span>
+            <span className="comment-count">{this.props.task.comments.length}</span>
+          </div>
+          <div className="edit-menu">
+              <i className="update fa fa-pencil-square" onClick={this.props.update.bind(this, this.props.task._id)}></i>
+              <i className="delete fa fa-trash" onClick={this.props.delete.bind(this, this.props.task._id)}></i>
+          </div>
+          <div className="show-comments-btn" onClick={this.toggleComments.bind(this)} >
+            <i className="img fa fa-caret-down"></i>
+            <i className="img fa fa-comment"></i>
+          </div>
+        </div>
+        {commentBox}
+      </div>
+
+
+    );
+  }
+}
