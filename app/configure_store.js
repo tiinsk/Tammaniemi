da2929646852fab877f1c1e6cbcbb8fda033dc9c
Aleksi <aleksi.kallio@outlook.com>
2016-11-17 20:01:46 +0200

    Add redux-dev-tools

--- app/configure_store.js
+++ app/configure_store.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from 'redux'
 import promiseMiddleware from 'redux-promise';
 import thunkMiddleware from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './reducers';
 
 export default function configureStore(initialState) {
-  const store = createStore(rootReducer, initialState, applyMiddleware(promiseMiddleware, thunkMiddleware));
+  const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(promiseMiddleware, thunkMiddleware)));
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
